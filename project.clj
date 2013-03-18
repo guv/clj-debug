42b727a01b690cca698b0d8fa534e677c636c2ae
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2013-03-18 14:18:01 +0100

    Upgrades used Clojure version and clj-gui.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject clj-debug "0.6.5"
+(defproject clj-debug "0.7.0"
   :description "Library for debugging Clojure programms with support for tracing, timing and inspection."
   :url "https://github.com/guv/clj-debug"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
 	:dependencies 
-	[[org.clojure/clojure "1.2.1"]
-   [org.clojure/core.incubator "0.1.0"]
-   [clj-gui "0.2.1"]]
+	[[org.clojure/clojure "1.5.1"]
+   [org.clojure/core.incubator "0.1.2"]
+   [clj-gui "0.3.0"]]
   :aot [debug.inspect.inspectable])
